69db18f1bd2e7ede4beb2c626240f672465ec20b
Jesse Carah <jessecarah@google.com>
2020-06-11 13:30:26 +0000

    removed a file

--- create_user.py
+++
@@ -1,9 +0,0 @@
-import looker_sdk
-
-
-sdk = looker_sdk.init31()  # or init40() for v4.0 API
-# and away you go
-my_user = sdk.me()
-new_user = looker_sdk.models.WriteUser(first_name="Jane", last_name="Doe")
-created_user = sdk.create_user(body=new_user)
-print(created_user)
